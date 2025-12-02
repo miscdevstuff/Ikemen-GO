c96d02d355155d3799c825f7195309b7acd2d8f1
Sohil876 <sohil876@gmail.com>
2025-12-02 23:05:45 +0530

    Fix issues

--- app_android/android/app/src/main/java/com/ikemenmobile/MainActivity.kt
+++ app_android/android/app/src/main/java/com/ikemenmobile/MainActivity.kt
@@ -4,4 +4,7 @@ import org.libsdl.app.SDLActivity
 
 class MainActivity : SDLActivity() {
     // SDL handles native main + GL + input
+    override fun getLibraries(): Array<String> {
+        return arrayOf("ikemen") // Loads libikemen.so
+    }
 }
