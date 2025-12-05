0b847d68df1659c4cc04a5ec8ab85bffd4d7fa3d
Sohil876 <sohil876@gmail.com>
2025-12-05 19:56:30 +0530

    Use gles conpatible math for vert shaders

--- src/shaders/model.vert.glsl
+++ src/shaders/model.vert.glsl
@@ -2,20 +2,21 @@
 precision highp float;
 precision mediump int;
 #endif
+
 #if __VERSION__ >= 450
 #define COMPAT_TEXTURE texture
 layout(binding = 0) uniform UniformBufferObject0 {
-	mat4 view, projection;
-	mat4 lightMatrices[4];
-	layout(offset = 688) vec3 cameraPosition;
+    mat4 view, projection;
+    mat4 lightMatrices[4];
+    layout(offset = 688) vec3 cameraPosition;
 };
 
 layout(binding = 2) uniform UniformBufferObject2 {
-	mat4 model,normalMatrix;
-	int numJoints,numTargets,morphTargetTextureDimension,numVertices;
-	vec4 morphTargetWeight[2];
-	vec4 morphTargetOffset;
-	float meshOutline;
+    mat4 model,normalMatrix;
+    int numJoints,numTargets,morphTargetTextureDimension,numVertices;
+    vec4 morphTargetWeight[2];
+    vec4 morphTargetOffset;
+    float meshOutline;
 };
 
 layout(binding = 3) uniform sampler2D jointMatrices;
@@ -61,7 +62,6 @@ uniform mat4 model, view, projection;
 uniform mat4 normalMatrix;
 uniform mat4 lightMatrices[4];
 uniform sampler2D jointMatrices;
-//uniform highp sampler2D morphTargetValues;
 uniform sampler2D morphTargetValues;
 uniform int numJoints;
 uniform int numTargets;
@@ -71,7 +71,7 @@ uniform vec4 morphTargetOffset;
 uniform int numVertices;
 uniform float meshOutline;
 uniform vec3 cameraPosition;
-//gl_VertexID is not available in 1.2
+// gl_VertexID is not available in 1.2
 COMPAT_ATTRIBUTE float vertexId;
 COMPAT_ATTRIBUTE vec3 position;
 COMPAT_ATTRIBUTE vec3 normalIn;
@@ -91,8 +91,9 @@ COMPAT_VARYING vec4 vColor;
 COMPAT_VARYING vec3 worldSpacePos;
 COMPAT_VARYING vec4 lightSpacePos[4];
 
-// *** patched: make comparison float-safe ***
-#define useJoint0 (weights_0.x + weights_0.y + weights_0.z + weights_0.w + weights_1.x + weights_1.y + weights_1.z + weights_1.w > 0.0)
+// float-safe comparison
+#define useJoint0 (weights_0.x + weights_0.y + weights_0.z + weights_0.w + \
+                   weights_1.x + weights_1.y + weights_1.z + weights_1.w > 0.0)
 
 const bool useJoint1 = true;
 const bool useNormal = true;
@@ -103,139 +104,157 @@ const bool useOutlineAttribute = true;
 
 
 mat4 getMatrixFromTexture(float index){
-	mat4 mat;
-	mat[0] = COMPAT_TEXTURE(jointMatrices,vec2(0.5/6.0,(index+0.5)/numJoints));
-	mat[1] = COMPAT_TEXTURE(jointMatrices,vec2(1.5/6.0,(index+0.5)/numJoints));
-	mat[2] = COMPAT_TEXTURE(jointMatrices,vec2(2.5/6.0,(index+0.5)/numJoints));
-	mat[3] = vec4(0,0,0,1);
-	return transpose(mat);
+    mat4 mat;
+    mat[0] = COMPAT_TEXTURE(jointMatrices,vec2(0.5/6.0,(index+0.5)/float(numJoints)));
+    mat[1] = COMPAT_TEXTURE(jointMatrices,vec2(1.5/6.0,(index+0.5)/float(numJoints)));
+    mat[2] = COMPAT_TEXTURE(jointMatrices,vec2(2.5/6.0,(index+0.5)/float(numJoints)));
+    mat[3] = vec4(0.0,0.0,0.0,1.0);
+    return transpose(mat);
 }
+
 mat4 getNormalMatrixFromTexture(float index){
-	mat4 mat;
-	mat[0] = COMPAT_TEXTURE(jointMatrices,vec2(3.5/6.0,(index+0.5)/numJoints));
-	mat[1] = COMPAT_TEXTURE(jointMatrices,vec2(4.5/6.0,(index+0.5)/numJoints));
-	mat[2] = COMPAT_TEXTURE(jointMatrices,vec2(5.5/6.0,(index+0.5)/numJoints));
-	mat[3] = vec4(0,0,0,1);
-	return transpose(mat);
+    mat4 mat;
+    mat[0] = COMPAT_TEXTURE(jointMatrices,vec2(3.5/6.0,(index+0.5)/float(numJoints)));
+    mat[1] = COMPAT_TEXTURE(jointMatrices,vec2(4.5/6.0,(index+0.5)/float(numJoints)));
+    mat[2] = COMPAT_TEXTURE(jointMatrices,vec2(5.5/6.0,(index+0.5)/float(numJoints)));
+    mat[3] = vec4(0.0,0.0,0.0,1.0);
+    return transpose(mat);
 }
+
 mat4 getJointMatrix(){
-	mat4 ret = mat4(0);
-	ret += weights_0.x*getMatrixFromTexture(joints_0.x);
-	ret += weights_0.y*getMatrixFromTexture(joints_0.y);
-	ret += weights_0.z*getMatrixFromTexture(joints_0.z);
-	ret += weights_0.w*getMatrixFromTexture(joints_0.w);
-	if(useJoint1){
-		ret += weights_1.x*getMatrixFromTexture(joints_1.x);
-		ret += weights_1.y*getMatrixFromTexture(joints_1.y);
-		ret += weights_1.z*getMatrixFromTexture(joints_1.z);
-		ret += weights_1.w*getMatrixFromTexture(joints_1.w);
-	}
-	if(ret == mat4(0.0)){
-		return mat4(1.0);
-	}
-	return ret;
+    mat4 ret = mat4(0.0);
+    ret += weights_0.x*getMatrixFromTexture(joints_0.x);
+    ret += weights_0.y*getMatrixFromTexture(joints_0.y);
+    ret += weights_0.z*getMatrixFromTexture(joints_0.z);
+    ret += weights_0.w*getMatrixFromTexture(joints_0.w);
+    if(useJoint1){
+        ret += weights_1.x*getMatrixFromTexture(joints_1.x);
+        ret += weights_1.y*getMatrixFromTexture(joints_1.y);
+        ret += weights_1.z*getMatrixFromTexture(joints_1.z);
+        ret += weights_1.w*getMatrixFromTexture(joints_1.w);
+    }
+    if(ret == mat4(0.0)){
+        return mat4(1.0);
+    }
+    return ret;
 }
+
 mat3 getJointNormalMatrix(){
-	mat4 ret = mat4(0);
-	vec4 w1 = useJoint1?weights_1:vec4(0);
-	ret += weights_0.x*getNormalMatrixFromTexture(joints_0.x);
-	ret += weights_0.y*getNormalMatrixFromTexture(joints_0.y);
-	ret += weights_0.z*getNormalMatrixFromTexture(joints_0.z);
-	ret += weights_0.w*getNormalMatrixFromTexture(joints_0.w);
-	ret += w1.x*getNormalMatrixFromTexture(joints_1.x);
-	ret += w1.y*getNormalMatrixFromTexture(joints_1.y);
-	ret += w1.z*getNormalMatrixFromTexture(joints_1.z);
-	ret += w1.w*getNormalMatrixFromTexture(joints_1.w);
-	if(ret == mat4(0.0)){
-		return mat3(1.0);
-	}
-	return mat3(ret);
+    mat4 ret = mat4(0.0);
+    vec4 w1 = useJoint1 ? weights_1 : vec4(0.0);
+    ret += weights_0.x*getNormalMatrixFromTexture(joints_0.x);
+    ret += weights_0.y*getNormalMatrixFromTexture(joints_0.y);
+    ret += weights_0.z*getNormalMatrixFromTexture(joints_0.z);
+    ret += weights_0.w*getNormalMatrixFromTexture(joints_0.w);
+    ret += w1.x*getNormalMatrixFromTexture(joints_1.x);
+    ret += w1.y*getNormalMatrixFromTexture(joints_1.y);
+    ret += w1.z*getNormalMatrixFromTexture(joints_1.z);
+    ret += w1.w*getNormalMatrixFromTexture(joints_1.w);
+    if(ret == mat4(0.0)){
+        return mat3(1.0);
+    }
+    return mat3(ret);
 }
+
+// ---- NEW: avoid '%' for GLES2 ----
+float getMorphTargetWeight(int idx) {
+    int vecIndex  = idx / 4;              // which vec4
+    int compIndex = idx - vecIndex * 4;   // 0..3
+
+    vec4 w = morphTargetWeight[vecIndex];
+    if (compIndex == 0)      return w.x;
+    else if (compIndex == 1) return w.y;
+    else if (compIndex == 2) return w.z;
+    else                     return w.w;
+}
+// ----------------------------------
+
 void main(void) {
-	texcoord = uv;
-	vColor = useVertColor ? vertColor : vec4(1.0, 1.0, 1.0, 1.0);
+    texcoord = uv;
+    vColor = useVertColor ? vertColor : vec4(1.0, 1.0, 1.0, 1.0);
 
-	vec4 pos = vec4(position, 1.0);
-	normal  = useNormal  ? normalIn           : vec3(0.0, 0.0, 0.0);
-	tangent = useTangent ? vec3(tangentIn)    : vec3(0.0, 0.0, 0.0);
-	vec4 outlineAttribute = useOutlineAttribute ? outlineAttributeIn : vec4(0.0);
+    vec4 pos = vec4(position, 1.0);
+    normal  = useNormal  ? normalIn        : vec3(0.0, 0.0, 0.0);
+    tangent = useTangent ? vec3(tangentIn) : vec3(0.0, 0.0, 0.0);
+    vec4 outlineAttribute = useOutlineAttribute ? outlineAttributeIn : vec4(0.0);
 
-	// *** patched: float-safe comparisons for morph targets ***
-	if(morphTargetWeight[0][0] != 0.0){
-		for(int idx = 0; idx < numTargets; ++idx)
-		{
-			float i = float(idx) * float(numVertices) + vertexId;
-			vec2 xy = vec2(
-			    (i + 0.5) / float(morphTargetTextureDimension) - floor(i / float(morphTargetTextureDimension)),
-			    (floor(i / float(morphTargetTextureDimension)) + 0.5) / float(morphTargetTextureDimension)
-			);
+    if (morphTargetWeight[0][0] != 0.0){
+        for (int idx = 0; idx < numTargets; ++idx)
+        {
+            float i = float(idx) * float(numVertices) + vertexId;
+            float dim = float(morphTargetTextureDimension);
 
-			float idxf = float(idx);
+            vec2 xy = vec2(
+                (i + 0.5) / dim - floor(i / dim),
+                (floor(i / dim) + 0.5) / dim
+            );
 
-			if(idxf < morphTargetOffset[0]){
-				pos += morphTargetWeight[idx/4][idx%4] * COMPAT_TEXTURE(morphTargetValues, xy);
-			}else if(idxf < morphTargetOffset[1]){
-				normal += morphTargetWeight[idx/4][idx%4] * vec3(COMPAT_TEXTURE(morphTargetValues, xy));
-			}else if(idxf < morphTargetOffset[2]){
-				tangent += morphTargetWeight[idx/4][idx%4] * vec3(COMPAT_TEXTURE(morphTargetValues, xy));
-			}else if(idxf < morphTargetOffset[3]){
-				texcoord += morphTargetWeight[idx/4][idx%4] * vec2(COMPAT_TEXTURE(morphTargetValues, xy));
-			}else{
-				vColor += morphTargetWeight[idx/4][idx%4] * COMPAT_TEXTURE(morphTargetValues, xy);
-			}
-		}
-	}
+            float idxf = float(idx);
+            float w = getMorphTargetWeight(idx);
 
-	if(useJoint0){
-		mat4 jointMatrix       = getJointMatrix();
-		mat3 jointNormalMatrix = getJointNormalMatrix();
+            if (idxf < morphTargetOffset[0]){
+                pos += w * COMPAT_TEXTURE(morphTargetValues, xy);
+            } else if (idxf < morphTargetOffset[1]){
+                normal += w * vec3(COMPAT_TEXTURE(morphTargetValues, xy));
+            } else if (idxf < morphTargetOffset[2]){
+                tangent += w * vec3(COMPAT_TEXTURE(morphTargetValues, xy));
+            } else if (idxf < morphTargetOffset[3]){
+                texcoord += w * vec2(COMPAT_TEXTURE(morphTargetValues, xy));
+            } else {
+                vColor += w * COMPAT_TEXTURE(morphTargetValues, xy);
+            }
+        }
+    }
 
-		normal = mat3(normalMatrix) * jointNormalMatrix * normal;
-		vec4 tmp2 = model * jointMatrix * pos;
+    if (useJoint0){
+        mat4 jointMatrix       = getJointMatrix();
+        mat3 jointNormalMatrix = getJointNormalMatrix();
+        normal = mat3(normalMatrix) * jointNormalMatrix * normal;
+        vec4 tmp2 = model * jointMatrix * pos;
 
-		if(outlineAttribute.w > 0.0){
-			vec3 p = normalize(mat3(normalMatrix) * outlineAttribute.xyz)
-			         * outlineAttribute.w * meshOutline
-			         * length(cameraPosition - tmp2.xyz);
-			tmp2.xyz += p;
-		}else{
-			vec3 p = normal * meshOutline * length(cameraPosition - tmp2.xyz);
-			tmp2.xyz += p;
-		}
+        if (outlineAttribute.w > 0.0){
+            vec3 p = normalize(mat3(normalMatrix) * outlineAttribute.xyz)
+                     * outlineAttribute.w * meshOutline
+                     * length(cameraPosition - tmp2.xyz);
+            tmp2.xyz += p;
+        } else {
+            vec3 p = normal * meshOutline * length(cameraPosition - tmp2.xyz);
+            tmp2.xyz += p;
+        }
 
-		gl_Position   = projection * view * tmp2;
-		worldSpacePos = vec3(tmp2);
-		for(int i = 0; i < 4; i++){
-			lightSpacePos[i] = lightMatrices[i] * tmp2;
-		}
-	}else{
-		if(normal.x + normal.y + normal.z != 0.0){
-			normal = normalize(mat3(normalMatrix) * normal);
-		}
-		if(tangent.x + tangent.y + tangent.z != 0.0){
-			tangent   = normalize(vec3(model * vec4(tangent, 0.0)));
-			// *** patched: use 0.0 to keep ternary float-typed ***
-			bitangent = cross(normal, tangent) * (useTangent ? tangentIn.w : 0.0);
-		}
-		vec4 tmp2 = model * pos;
-		if(outlineAttribute.w > 0.0){
-			vec3 p = normalize(mat3(normalMatrix) * outlineAttribute.xyz)
-			         * outlineAttribute.w * meshOutline
-			         * length(cameraPosition - tmp2.xyz);
-			tmp2.xyz += p;
-		}else{
-			vec3 p = normal * meshOutline * length(cameraPosition - tmp2.xyz);
-			tmp2.xyz += p;
-		}
+        gl_Position   = projection * view * tmp2;
+        worldSpacePos = vec3(tmp2);
+        for (int i = 0; i < 4; i++){
+            lightSpacePos[i] = lightMatrices[i] * tmp2;
+        }
+    } else {
+        if (normal.x + normal.y + normal.z != 0.0){
+            normal = normalize(mat3(normalMatrix) * normal);
+        }
+        if (tangent.x + tangent.y + tangent.z != 0.0){
+            tangent   = normalize(vec3(model * vec4(tangent, 0.0)));
+            bitangent = cross(normal, tangent) * (useTangent ? tangentIn.w : 0.0);
+        }
 
-		gl_Position   = projection * view * tmp2;
-		worldSpacePos = vec3(tmp2);
-		for(int i = 0; i < 4; i++){
-			lightSpacePos[i] = lightMatrices[i] * tmp2;
-		}
-	}
+        vec4 tmp2 = model * pos;
+        if (outlineAttribute.w > 0.0){
+            vec3 p = normalize(mat3(normalMatrix) * outlineAttribute.xyz)
+                     * outlineAttribute.w * meshOutline
+                     * length(cameraPosition - tmp2.xyz);
+            tmp2.xyz += p;
+        } else {
+            vec3 p = normal * meshOutline * length(cameraPosition - tmp2.xyz);
+            tmp2.xyz += p;
+        }
 
-	#if __VERSION__ >= 450
-	gl_Position.y = -gl_Position.y;
-	#endif
+        gl_Position   = projection * view * tmp2;
+        worldSpacePos = vec3(tmp2);
+        for (int i = 0; i < 4; i++){
+            lightSpacePos[i] = lightMatrices[i] * tmp2;
+        }
+    }
+
+    #if __VERSION__ >= 450
+    gl_Position.y = -gl_Position.y;
+    #endif
 }
