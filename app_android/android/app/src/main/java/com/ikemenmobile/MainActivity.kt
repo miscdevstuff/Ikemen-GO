af2e921de81cf782bea1ae65ac2392c0f462eff8
Sohil876 <sohil876@gmail.com>
2025-12-05 20:34:36 +0530

    Fix orientation to landscape, build sdl withaudio, revert libname change in sdlactivity and override from mainactivity

--- app_android/android/app/src/main/java/com/ikemenmobile/MainActivity.kt
+++ app_android/android/app/src/main/java/com/ikemenmobile/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.ikemenmobile
 
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.os.Bundle
 import org.libsdl.app.SDLActivity
 
@@ -8,11 +10,28 @@ class MainActivity : SDLActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Force landscape once on startup
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE
+
         // Before SDL / Go touches basePath, make sure assets are extracted there.
         AssetsExtractor.ensureAssetsExtracted(this)
     }
+
+    override fun onResume() {
+        super.onResume()
+        // Re-assert landscape when returning to the app
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        // If the system tries to flip us, push back to landscape
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE
+    }
+
     // SDL handles native main + GL + input
-    // Already renamed main to ikemen in sdl code itself
+    // Already renamed main to ikemen in SDL code itself
     override fun getLibraries(): Array<String> {
         return arrayOf("ikemen") // Loads libikemen.so
     }
