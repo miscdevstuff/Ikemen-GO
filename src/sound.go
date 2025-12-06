1741eea32f29fe182c0d797536284d4d3da1662b
Sohil876 <sohil876@gmail.com>
2025-12-06 17:31:24 +0530

    Try tuning audio config

--- src/sound.go
+++ src/sound.go
@@ -33,8 +33,8 @@ import (
 )
 
 const (
-	audioOutLen          = 4096
-	audioFrequency       = 44100
+	audioOutLen          = 8196
+	audioFrequency       = 22050
 	audioPrecision       = 4
 	audioResampleQuality = 1
 	audioSoundFont       = "sound/soundfont.sf2" // default path for MIDI soundfont
