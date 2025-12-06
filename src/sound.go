f3e270c7b1fed3dbc377ba9fd95626aa85a3dd21
Sohil876 <sohil876@gmail.com>
2025-12-06 18:31:47 +0530

    Try to implement proper thread priority to fix audio, update audio config

--- src/sound.go
+++ src/sound.go
@@ -33,8 +33,8 @@ import (
 )
 
 const (
-	audioOutLen          = 8196
-	audioFrequency       = 22050
+	audioOutLen          = 4096
+	audioFrequency       = 44100
 	audioPrecision       = 4
 	audioResampleQuality = 1
 	audioSoundFont       = "sound/soundfont.sf2" // default path for MIDI soundfont
