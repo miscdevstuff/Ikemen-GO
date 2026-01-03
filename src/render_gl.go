43d1fff6975a33519f17321753c98afc1b96c068
Sohil876 <sohil876@gmail.com>
2026-01-03 20:30:17 +0530

    Revert android mode fix on gl renderer

--- src/render_gl.go
+++ src/render_gl.go
@@ -86,16 +86,8 @@ func (s *ShaderProgram_GL21) RegisterTextures(names ...string) {
 
 func (r *Renderer_GL21) compileShader(shaderType uint32, src string) (shader uint32, err error) {
 	shader = gl.CreateShader(shaderType)
-	
-	// FIX: Use GLES 2.0 (#version 100) on Android to allow precision qualifiers.
-	// Desktop GL 2.1 uses #version 120 (where precision is illegal).
-	versionHeader := "#version 120\n"
-	if runtime.GOOS == "android" {
-		versionHeader = "#version 100\n"
-	}
-	
-	src = versionHeader + src + "\x00"
-	
+	src = "#version 120\n" + src + "\x00""
+
 	s, _ := gl.Strs(src)
 	var l int32 = int32(len(src) - 1)
 	gl.ShaderSource(shader, 1, s, &l)
